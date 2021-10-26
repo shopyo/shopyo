e5a38b4ce529c60de6342e97c7951c3f7fa50102
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2021-10-26 11:21:51 +0400

    bump: 4.1.3

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ if sys.argv[-1] == "publish":  # requests
 setup(
     install_requires=open(os.path.join(here, "requirements.txt"), encoding="utf-8")
     .read()
-    .split("\n")
+    .split("\n"),
+    url="https://github.com/shopyo/shopyo",
 )
