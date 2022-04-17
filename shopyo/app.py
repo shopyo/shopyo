c3579dadc0edee63e0c8e7730a8c6481649dc6b8
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-04-17 11:02:59 +0400

    fix: app.py theme template locations

--- shopyo/app.py
+++ shopyo/app.py
@@ -196,13 +196,15 @@ def setup_theme_paths(app):
         back_theme_dir = os.path.join(
             app.config["BASE_DIR"], "static", "themes", "back"
         )
-        my_loader = jinja2.ChoiceLoader(
-            [
-                app.jinja_loader,
-                jinja2.FileSystemLoader([front_theme_dir, back_theme_dir]),
-            ]
-        )
-        app.jinja_loader = my_loader
+
+        if os.path.exists(front_theme_dir) and os.path.exists(back_theme_dir):
+            my_loader = jinja2.ChoiceLoader(
+                [
+                    app.jinja_loader,
+                    jinja2.FileSystemLoader([front_theme_dir, back_theme_dir]),
+                ]
+            )
+            app.jinja_loader = my_loader
 
 
 def inject_global_vars(app, global_template_variables):
