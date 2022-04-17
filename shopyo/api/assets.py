06d238c7d5615c1b190da3110828f9a8b7155191
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2022-04-17 10:26:20 +0400

    refactor: devstatic moved to api

--- shopyo/api/assets.py
+++ shopyo/api/assets.py
@@ -1,4 +1,7 @@
+import os
+
 from flask import current_app
+from flask import send_from_directory
 from flask import url_for
 
 
@@ -19,3 +22,30 @@ def get_static(boxormodule, filename):
         return url_for("devstatic", boxormodule=boxormodule, path=filename)
     else:
         return url_for("static", path=f"modules/{boxormodule}/{filename}")
+
+
+def register_devstatic(app, modules_path):
+    """
+    Resgisters endpoint for serving files in debug mode
+
+
+    Args:
+        app (Flask app)
+
+    Returns
+    -------
+    URL for static file in debug mode
+    """
+
+    @app.route("/devstatic/<path:boxormodule>/f/<path:path>")
+    def devstatic(boxormodule, path):
+        if app.config["DEBUG"]:
+            if boxormodule.startswith("box__"):
+                box = boxormodule.split("/")[0]
+                module = boxormodule.split("/")[1]
+                module_static = os.path.join(modules_path, box, module, "static")
+                return send_from_directory(module_static, path=path)
+            else:
+                module = boxormodule
+                module_static = os.path.join(modules_path, module, "static")
+                return send_from_directory(module_static, path=path)
