0ce3df0589c1bcca7236f2343f37644dbda009d2
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-09-28 09:40:40 +0400

    feat: appadmin as separate package

---
+++ default_packages/shopyo_appadmin/shopyo_appadmin/admin.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+from flask import redirect
+from flask_login import current_user
+from init import login_manager
+from shopyo_auth.models import User
+
+from shopyo.api.html import notify_warning
+
+login_manager.login_message = notify_warning("Please login for access")
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
+
+
+def admin_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if current_user.is_admin:
+            return f(*args, **kwargs)
+        else:
+            return redirect("/")
+
+    return wrap
