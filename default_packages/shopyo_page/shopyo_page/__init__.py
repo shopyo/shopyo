2782a210a91de651d21893cb5bbb064a2b216a21
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-09-30 11:05:56 +0400

    feat: convert page to shopyo_page

---
+++ default_packages/shopyo_page/shopyo_page/__init__.py
@@ -0,0 +1,28 @@
+from typing import Any
+import os 
+import json
+from flask import Flask
+from shopyo_page.view import module_blueprint
+
+__version__ = "1.0.1"
+
+info = {}
+with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
+    info = json.load(f)
+
+class ShopyoPage:
+    def __init__(self, app: Any = None) -> None:
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app: Flask) -> None:
+        if not hasattr(app, "extensions"):
+            app.extensions = {}
+
+        app.extensions["shopyo_page"] = self
+        bp = module_blueprint
+        app.register_blueprint(bp)
+
+
+    def get_info(self):
+        return info
