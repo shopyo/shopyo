6875e4952053e4f9944b0a76c30d6232a7d765f8
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-10-03 17:32:35 +0400

    chore: rm settings theme modules

--- shopyo/init.py
+++ shopyo/init.py
@@ -22,9 +22,22 @@ csrf = CSRFProtect()
 login_manager = LoginManager()
 
 
+import importlib
+
 def load_extensions(app):
     migrate.init_app(app, db)
     db.init_app(app)
     mail.init_app(app)
     login_manager.init_app(app)
     csrf.init_app(app)
+
+
+    with app.app_context():
+        for plugin in app.extensions:
+            if plugin.startswith("shopyo_"):
+                try:
+                    module = importlib.import_module(f'{plugin}.models')
+                    for attr_name in dir(module):
+                        attr = getattr(module, attr_name)
+                except Exception as e:
+                    print(e)
