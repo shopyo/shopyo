a933fb6d870223c10b0ba0aeba2876760fa63ee2
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-10-06 08:58:18 +0400

    feat(pack): options added to theme

--- default_packages/shopyo_theme/shopyo_theme/__init__.py
+++ default_packages/shopyo_theme/shopyo_theme/__init__.py
@@ -12,6 +12,9 @@ info = {}
 with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
     info = json.load(f)
 
+default_config = {
+    'SHOPYO_THEME_URL': '/shopyo-theme'
+}
 
 class ShopyoTheme:
     def __init__(self, app: Any = None) -> None:
@@ -33,9 +36,12 @@ class ShopyoTheme:
         if not hasattr(app, "extensions"):
             app.extensions = {}
 
+        for key, value in default_config.items():
+            app.config.setdefault(key, value)
+
         app.extensions["shopyo_theme"] = self
         bp = module_blueprint
-        app.register_blueprint(bp)
+        app.register_blueprint(bp, url_prefix=app.config['SHOPYO_THEME_URL'])
         app.jinja_env.globals["shopyo_theme"] = self
 
     def get_info(self):
