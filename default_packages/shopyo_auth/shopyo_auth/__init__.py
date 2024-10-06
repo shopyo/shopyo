c134484614c8d2da5a9e3716ced85d18e3779427
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-10-06 08:57:35 +0400

    feat(pack): options added to auth

--- default_packages/shopyo_auth/shopyo_auth/__init__.py
+++ default_packages/shopyo_auth/shopyo_auth/__init__.py
@@ -13,6 +13,10 @@ with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as 
     info = json.load(f)
 
 
+default_config = {
+    'SHOPYO_AUTH_URL': '/shopyo-auth'
+}
+
 class ShopyoAuth:
     def __init__(self, app: Any = None) -> None:
         if app is not None:
@@ -23,9 +27,13 @@ class ShopyoAuth:
         if not hasattr(app, "extensions"):
             app.extensions = {}
 
+        for key, value in default_config.items():
+            app.config.setdefault(key, value)
+
         app.extensions["shopyo_auth"] = self
         bp = module_blueprint
-        app.register_blueprint(bp)
+        app.register_blueprint(bp, url_prefix=app.config['SHOPYO_AUTH_URL'])
+        app.jinja_env.globals["shopyo_auth"] = self
 
     def get_info(self):
         return info
