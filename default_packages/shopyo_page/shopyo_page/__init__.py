3ca904e668e34285e1a47858f45e97cabc48be5a
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-10-06 08:58:07 +0400

    feat(pack): options added to page

--- default_packages/shopyo_page/shopyo_page/__init__.py
+++ default_packages/shopyo_page/shopyo_page/__init__.py
@@ -12,6 +12,11 @@ with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as 
     info = json.load(f)
 
 
+default_config = {
+    'SHOPYO_PAGE_URL': '/shopyo-page'
+}
+
+
 class ShopyoPage:
     def __init__(self, app: Any = None) -> None:
         if app is not None:
@@ -23,9 +28,12 @@ class ShopyoPage:
         if not hasattr(app, "extensions"):
             app.extensions = {}
 
+        for key, value in default_config.items():
+            app.config.setdefault(key, value)
+
         app.extensions["shopyo_page"] = self
         bp = module_blueprint
-        app.register_blueprint(bp)
+        app.register_blueprint(bp, url_prefix=app.config['SHOPYO_PAGE_URL'])
         app.jinja_env.globals["shopyo_page"] = self
 
     def get_info(self):
