b5436072ade593cb59d68850747c9707fd08e590
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-10-06 08:57:21 +0400

    feat(pack): options added to appadmin

--- default_packages/shopyo_appadmin/shopyo_appadmin/__init__.py
+++ default_packages/shopyo_appadmin/shopyo_appadmin/__init__.py
@@ -11,6 +11,9 @@ info = {}
 with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
     info = json.load(f)
 
+default_config = {
+    'SHOPYO_APPADMIN_URL': '/shopyo-appadmin'
+}
 
 class ShopyoAppAdmin:
     def __init__(self, app: Any = None) -> None:
@@ -21,9 +24,12 @@ class ShopyoAppAdmin:
         if not hasattr(app, "extensions"):
             app.extensions = {}
 
+        for key, value in default_config.items():
+            app.config.setdefault(key, value)
+
         app.extensions["shopyo_appadmin"] = self
         bp = module_blueprint
-        app.register_blueprint(bp)
+        app.register_blueprint(bp, url_prefix=app.config['SHOPYO_APPADMIN_URL'])
 
     def get_info(self):
         return info
