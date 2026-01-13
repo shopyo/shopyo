f5e48f2382aceee948a15e3b334ddb7c461ce8b7
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2026-01-13 12:58:09 +0400

    feat:  url config var

--- default_packages/shopyo_base/shopyo_base/__init__.py
+++ default_packages/shopyo_base/shopyo_base/__init__.py
@@ -5,12 +5,14 @@ import json
 from flask import Flask
 from shopyo_base.view import module_blueprint
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 
 info = {}
 with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + "info.json") as f:
     info = json.load(f)
+
+default_config = {"SHOPYO_BASE_NAV_LOGIN": True, "SHOPYO_BASE_NAV_REGISTER": True}
 
 
 class ShopyoBase:
@@ -21,6 +23,9 @@ class ShopyoBase:
     def init_app(self, app: Flask) -> None:
         if not hasattr(app, "extensions"):
             app.extensions = {}
+
+        for key, value in default_config.items():
+            app.config.setdefault(key, value)
 
         app.extensions["shopyo_base"] = self
         bp = module_blueprint
