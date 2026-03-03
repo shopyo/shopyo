564036a8e538ce7c06f663bf007631aa9fcd971b
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2026-03-04 00:36:24 +0400

    fixes for default_packages

--- default_packages/shopyo_appadmin/shopyo_appadmin/admin.py
+++ default_packages/shopyo_appadmin/shopyo_appadmin/admin.py
@@ -2,7 +2,7 @@ from functools import wraps
 
 from flask import redirect
 from flask_login import current_user
-from init import login_manager
+from init import db, login_manager
 from shopyo_auth.models import User
 
 from shopyo.api.html import notify_warning
@@ -12,7 +12,7 @@ from shopyo.api.html import notify_warning
 
 @login_manager.user_loader
 def load_user(id):
-    return User.query.get(id)
+    return db.session.get(User, id)
 
 
 def admin_required(f):
