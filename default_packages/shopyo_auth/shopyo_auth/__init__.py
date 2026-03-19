08cc4976e5da7c994516e9470cb23ad3d4da5c5e
Abdur-Rahmaan Janhangeer <cryptolabour@gmail.com>
2026-03-19 21:26:50 +0400

    pkg bump

--- default_packages/shopyo_auth/shopyo_auth/__init__.py
+++ default_packages/shopyo_auth/shopyo_auth/__init__.py
@@ -9,7 +9,7 @@ from flask.cli import with_appcontext
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-__version__ = "1.7.1"
+__version__ = "1.8.0"
 
 limiter = Limiter(key_func=get_remote_address)
 
